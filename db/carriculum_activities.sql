1792f449f37259db3ff65cedcef7102e0bb4dceb
ishan <ishanjmails@gmail.com>
2017-02-03 19:49:54 +1300

    remove unwanted

--- db/carriculum_activities.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO `CurriculumActivity` (`ClassName`, `LastEdited`, `Created`,`Title`) VALUES
-('CurriculumActivity','2016-09-16 10:22:19','2016-09-16 10:22:19', 'Interact Club'),
-('CurriculumActivity','2016-09-16 10:22:19','2016-09-16 10:22:19', 'Mathematics Society'),
-('CurriculumActivity','2016-09-16 10:22:19','2016-09-16 10:22:19', 'Drama Society'),
-('CurriculumActivity','2016-09-16 10:22:19','2016-09-16 10:22:19', 'Computer Society'),
-('CurriculumActivity','2016-09-16 10:22:19','2016-09-16 10:22:19', 'Commerce Society'),
-('CurriculumActivity','2016-09-16 10:22:19','2016-09-16 10:22:19', 'Science Association'),
-('CurriculumActivity','2016-09-16 10:22:19','2016-09-16 10:22:19', 'Choir Society');
